e2fb1420ec6b492d8939ed457217a38759f5710c
pkphajiwon <31185488+pkphajiwon@users.noreply.github.com>
2019-10-22 22:53:00 +0700

    w

--- app/src/main/java/ducku/com/moneyhappy/ManHinhDangNhap.java
+++ app/src/main/java/ducku/com/moneyhappy/ManHinhDangNhap.java
@@ -1,12 +1,14 @@
 package ducku.com.moneyhappy;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ManHinhDangNhap extends AppCompatActivity {
-
+    Button btnDangNhap;
     EditText EditTextMK;
     EditText EditTextSDT;
     @Override
@@ -18,11 +20,25 @@ public class ManHinhDangNhap extends AppCompatActivity {
 //        addEvent();
     }
 
-//    private void addEvent() {
-//    }
+    private void addEvent() {
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new layketqua().execute("name=hajiwon1023");
+            }
+        });
+    }
 
     private void addControls() {
         EditTextMK  = findViewById(R.id.editTextMK);
         EditTextSDT = findViewById(R.id.editTextSDT);
+        btnDangNhap = findViewById(R.id.btnDangNhap);
+    }
+
+    private class layketqua extends api {
+        @Override
+        protected void onPostExecute(String s) {
+            EditTextSDT.setText(s);
+        }
     }
 }
