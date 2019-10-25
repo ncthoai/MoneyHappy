6a88d84eecb31843fe1cc38448039011a178dd26
pkphajiwon <31185488+pkphajiwon@users.noreply.github.com>
2019-10-26 06:20:42 +0700

    won

--- app/src/main/java/ducku/com/moneyhappy/ManHinhDangKy.java
+++ app/src/main/java/ducku/com/moneyhappy/ManHinhDangKy.java
@@ -4,23 +4,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ManHinhDangKy extends AppCompatActivity {
 
     Button btnDangKy;
     EditText EditTextSDT;
+    TextView twThongBao, twTieuDeKhung;
+    String Status = "phone";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
-        //addEvent();
+
         addControls();
-
-
-        new layketqua().execute("name=hajiwon1023");
+        addEvent();
     }
 
     private void addEvent() {
@@ -28,20 +33,61 @@ public class ManHinhDangKy extends AppCompatActivity {
         btnDangKy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //do ect
+                String QueryString = "act=register&"+Status+"="+EditTextSDT.getText().toString();
+                Toast.makeText(ManHinhDangKy.this, "query string:\n\n"+ QueryString+"\n\n", Toast.LENGTH_SHORT).show();
+                new goiDangKy().execute(QueryString);
             }
         });
 
     }
     private void addControls() {
-        btnDangKy   = findViewById(R.id.btnDangKy);
-        EditTextSDT = findViewById(R.id.editTextSDT);
+        btnDangKy     = findViewById(R.id.btnDangKy);
+        EditTextSDT   = findViewById(R.id.editTextSDT);
+        twThongBao    = findViewById(R.id.twThongBao);
+        twTieuDeKhung = findViewById(R.id.twSDT);
     }
 
-    private class layketqua extends api {
+    private class goiDangKy extends api {
         @Override
         protected void onPostExecute(String s) {
-            EditTextSDT.setText(s);
+            JSONObject obj = null;
+            try {
+                obj = new JSONObject(s);
+                String result = obj.getString("result");
+
+                // Xu ly dang ky sdt
+                if(Status.equals("phone")){
+                    if(result.equals("OTP")){
+                        //bla bla
+                        twThongBao.   setText("Da gui ma OTP den "+EditTextSDT.getText().toString());
+                        twTieuDeKhung.setText("Nhap ma OTP:");
+                        btnDangKy.    setText("Xác Nhận");
+                        Status = "phone="+EditTextSDT.getText().toString()+"&otp";
+                        EditTextSDT.  setText("");
+
+                    } else if(result.equals("ALREADY_EXIST")) {
+                        twThongBao.setText("SDT da ton tai");
+
+                    }else {
+                        twThongBao.setText("Khong co ket noi den sv");
+
+                    }
+                }
+                //Xu ly kich haot tai khoan
+                else {
+                    if(result.equals("true")) {
+                        twThongBao.setText("Kich hoat thanh cong -> di den tao mk");
+                    }
+                    else{
+                        twThongBao.setText("Sai ma OTP");
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Toast.makeText(ManHinhDangKy.this, "WEBSERVER-re:\n\n"+s+"\n\n", Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/ducku/com/moneyhappy/ManHinhDangNhap.java
+++ app/src/main/java/ducku/com/moneyhappy/ManHinhDangNhap.java
@@ -1,15 +1,27 @@
 package ducku.com.moneyhappy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManHinhDangNhap extends AppCompatActivity {
 
     EditText txtSDT,txtPassword;
+    TextView twMsg;
+    Button btnDangNhap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -19,11 +31,18 @@ public class ManHinhDangNhap extends AppCompatActivity {
         addControls();
         addEvent();
 
-
     }
 
     private void addEvent() {
 
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tempSDT      = txtSDT.getText().toString();
+                String tempPassword = txtPassword.getText().toString();
+                new GoiDangNhap().execute("act=login&username="+tempSDT+"&password="+tempPassword);
+            }
+        });
     }
 
     private void addControls() {
@@ -31,6 +50,35 @@ public class ManHinhDangNhap extends AppCompatActivity {
 
         txtSDT.setSelection(0);
         txtPassword=findViewById(R.id.txtPassword);
+
+        twMsg = findViewById(R.id.textView2);
+
+        btnDangNhap = findViewById(R.id.btnDangNhap);
+    }
+
+    private class GoiDangNhap extends api {
+        @Override
+        protected void onPostExecute(String s) {
+
+            JSONObject obj = null;
+            try {
+                obj = new JSONObject(s);
+                String result = obj.getString("result");
+                if(result.equals("true")){
+                    //bla bla
+                    twMsg.setText("Dang nhap thanh cong");
+                } else {
+                    //bla bla
+                    twMsg.setText("Thong tin khong chinh xac \n Du lieu mau: PHONE/PW: 0329571692/123456");
+                }
+                //xoa pass
+                txtPassword.setText("");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
 
