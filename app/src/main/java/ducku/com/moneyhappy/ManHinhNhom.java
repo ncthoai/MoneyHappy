db98e462df5b75cde4eb48c968084d948bbe1c85
vinhduc98 <37188845+vinhduc98@users.noreply.github.com>
2019-10-31 22:27:56 +0700

    updateManHinhNhom

--- app/src/main/java/ducku/com/moneyhappy/ManHinhNhom.java
+++ app/src/main/java/ducku/com/moneyhappy/ManHinhNhom.java
@@ -1,16 +1,11 @@
 package ducku.com.moneyhappy;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ListView;
-import android.widget.TabHost;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,7 +16,7 @@ import java.util.ArrayList;
 import ducku.com.moneyhappy.adapter.CategoryAdapter;
 import ducku.com.moneyhappy.model.Category;
 
-public class ManHinhThuChi extends AppCompatActivity {
+public class ManHinhNhom extends AppCompatActivity {
 
     ListView lvCategoryChi, lvCategoryThu;
     ArrayList<Category> arrayCategory, arrayCategory2;
@@ -30,75 +25,27 @@ public class ManHinhThuChi extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_man_hinh_thu_chi);
-
-        Toolbar tb =  findViewById(R.id.tb2);
-        setSupportActionBar(tb);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setTitle("Củ cải");
-
-
-        final TabHost tabHost=findViewById(R.id.tabhost);
-        tabHost.setup();
-
-        final TabHost.TabSpec tab1=tabHost.newTabSpec("t1");
-        tab1.setIndicator("Thu tiền");
-        tab1.setContent(R.id.tab1);
-        tabHost.addTab(tab1);
-
-        TabHost.TabSpec tab2=tabHost.newTabSpec("t2");
-        tab2.setIndicator("Chi tiền");
-        tab2.setContent(R.id.tab2);
-        tabHost.addTab(tab2);
+        setContentView(R.layout.activity_man_hinh_nhom);
 
         addControls();
         addEvents();
         new GetCategoryChi().execute("act=getcategory&iduser=1&type=0");
         new GetCategoryThu().execute("act=getcategory&iduser=1&type=1");
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        getMenuInflater().inflate(R.menu.mymenu,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
-            case android.R.id.home:
-                Intent intent= new Intent(ManHinhThuChi.this,ManHinhChiTien.class);
-                startActivity(intent);
-                break;
-            case R.id.menu1:
-                //code xử lý khi bấm menu1
-                break;
-            case R.id.menu2:
-                //code xử lý khi bấm menu2
-                break;
-            case R.id.menu4:
-                //code xử lý khi bấm menu3
-                break;
-            default:break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    private void addEvents() {
     }
 
     private void addControls() {
-        lvCategoryChi =  findViewById(R.id.lvchi);
-        lvCategoryThu=findViewById(R.id.lvthu);
+        lvCategoryChi =  findViewById(R.id.lvKhoanChi);
+        lvCategoryThu=findViewById(R.id.lvkhoanthu);
 
         arrayCategory = new ArrayList<>();
         arrayCategory2 = new ArrayList<>();
 
         res = getResources();
+    }
+
+    private void addEvents()
+    {
+
     }
 
     private class GetCategoryChi extends api {
@@ -117,7 +64,7 @@ public class ManHinhThuChi extends AppCompatActivity {
                     arrayCategory2.add(new Category(id,parent_id,idImg,name));
                     Log.d("Log",name);
                 }
-                adapter2 = new CategoryAdapter(ManHinhThuChi.this, 0, arrayCategory2);
+                adapter2 = new CategoryAdapter(ManHinhNhom.this, 0, arrayCategory2);
                 lvCategoryChi.setAdapter(adapter2);
 
             } catch (JSONException e) {
@@ -143,7 +90,7 @@ public class ManHinhThuChi extends AppCompatActivity {
 
                     Log.d("Log",name);
                 }
-                adapter = new CategoryAdapter(ManHinhThuChi.this, 0, arrayCategory);
+                adapter = new CategoryAdapter(ManHinhNhom.this, 0, arrayCategory);
                 lvCategoryThu.setAdapter(adapter);
 
             } catch (JSONException e) {
