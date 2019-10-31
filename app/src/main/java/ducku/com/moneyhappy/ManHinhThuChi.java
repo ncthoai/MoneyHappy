4e2f1c6b68fc3ad091f7ceedd8b207305295bba3
Cong Thoai <thoai.nguyen@onlinecm.vn>
2019-11-01 00:59:55 +0700

    [ThuChi] hợp nhất biến cho dễ xử lý sau này

--- app/src/main/java/ducku/com/moneyhappy/ManHinhThuChi.java
+++ app/src/main/java/ducku/com/moneyhappy/ManHinhThuChi.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.TabHost;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,22 +24,39 @@ import ducku.com.moneyhappy.model.Category;
 
 public class ManHinhThuChi extends AppCompatActivity {
 
-    ListView lvCategoryChi, lvCategoryThu;
-    ArrayList<Category> arrayCategory, arrayCategory2;
-    CategoryAdapter adapter, adapter2;
+    ListView lvCategory;
+    ArrayList<Category> arrayCategory;
+    CategoryAdapter adapter;
     Resources res;
+    TabHost tabHost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_hinh_thu_chi);
 
-        Toolbar tb =  findViewById(R.id.tb2);
-        setSupportActionBar(tb);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setTitle("Củ cải");
+        addControls();
 
+        addEvents();
+    }
 
-        final TabHost tabHost=findViewById(R.id.tabhost);
+    private void addEvents() {
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                if(tabId.equals("t1")) {
+                    new GetCategory().execute("act=getcategory&iduser=1&type=1");
+                }
+                else {
+                    new GetCategory().execute("act=getcategory&iduser=1&type=0");
+                }
+            }
+        });
+    }
+
+    private void addControls() {
+        tabHost= findViewById(R.id.tabhost);
+
         tabHost.setup();
 
         final TabHost.TabSpec tab1=tabHost.newTabSpec("t1");
@@ -51,86 +69,23 @@ public class ManHinhThuChi extends AppCompatActivity {
         tab2.setContent(R.id.tab2);
         tabHost.addTab(tab2);
 
-        addControls();
-        addEvents();
-        new GetCategoryChi().execute("act=getcategory&iduser=1&type=0");
-        new GetCategoryThu().execute("act=getcategory&iduser=1&type=1");
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        getMenuInflater().inflate(R.menu.mymenu,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
-            case android.R.id.home:
-                Intent intent= new Intent(ManHinhThuChi.this,ManHinhChiTien.class);
-                startActivity(intent);
-                break;
-            case R.id.menu1:
-                //code xử lý khi bấm menu1
-                break;
-            case R.id.menu2:
-                //code xử lý khi bấm menu2
-                break;
-            case R.id.menu4:
-                //code xử lý khi bấm menu3
-                break;
-            default:break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    private void addEvents() {
-    }
-
-    private void addControls() {
-        lvCategoryChi =  findViewById(R.id.lvchi);
-        lvCategoryThu=findViewById(R.id.lvthu);
+        lvCategory=findViewById(R.id.lv_load_category);
 
         arrayCategory = new ArrayList<>();
-        arrayCategory2 = new ArrayList<>();
 
         res = getResources();
+
+        // Set defult load is tab thu and Listview Thu
+        tabHost.setCurrentTab(0);
+        new GetCategory().execute("act=getcategory&iduser=1&type=1");
     }
 
-    private class GetCategoryChi extends api {
+    private class GetCategory extends api {
         @Override
         protected void onPostExecute(String s) {
 
             try {
-                JSONArray array = new JSONArray(s);
-                for(int i=0;i<array.length();i++)
-                {
-                    JSONObject obcategory=array.getJSONObject(i);
-                    int id=obcategory.getInt("cid");
-                    int parent_id=obcategory.getInt("parent");
-                    String name=obcategory.getString("cname");
-                    int idImg=res.getIdentifier(obcategory.getString("img"),"drawable",getPackageName());
-                    arrayCategory2.add(new Category(id,parent_id,idImg,name));
-                    Log.d("Log",name);
-                }
-                adapter2 = new CategoryAdapter(ManHinhThuChi.this, 0, arrayCategory2);
-                lvCategoryChi.setAdapter(adapter2);
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private class GetCategoryThu extends api {
-        @Override
-        protected void onPostExecute(String s) {
-
-            try {
+                arrayCategory = new ArrayList<>();
                 JSONArray array = new JSONArray(s);
                 for(int i=0;i<array.length();i++)
                 {
@@ -140,11 +95,10 @@ public class ManHinhThuChi extends AppCompatActivity {
                     String name=obcategory.getString("cname");
                     int idImg=res.getIdentifier(obcategory.getString("img"),"drawable",getPackageName());
                     arrayCategory.add(new Category(id,parent_id,idImg,name));
-
                     Log.d("Log",name);
                 }
                 adapter = new CategoryAdapter(ManHinhThuChi.this, 0, arrayCategory);
-                lvCategoryThu.setAdapter(adapter);
+                lvCategory.setAdapter(adapter);
 
             } catch (JSONException e) {
                 e.printStackTrace();
